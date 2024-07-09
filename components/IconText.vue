5fe9834e92649981448b7e5213c0256b3f1979e5
imba97 <mail@imba97.cn>
2024-07-09 19:57:35 +0800

    perf: add click event to IconText component

--- components/IconText.vue
+++ components/IconText.vue
@@ -3,17 +3,20 @@ const props = defineProps<{
   iconClass: string
   href?: string
 }>()
+
+function onClick() {
+  if (props.href) {
+    window.open(props.href, '_blank')
+  }
+}
 </script>
 
 <template>
   <div
-    class="icon-text"
-    :class="[
+    class="icon-text" :class="[
       props.href ? 'cursor-pointer hover:bg-[rgba(0,0,0,0.2)] active:scale-95' : '',
-    ]"
-    mx="0.5" px-2 py-1 rounded-2 flex items-center justify-center gap-1 flex-inline
-    transition="all duration-300"
-    box-border
+    ]" mx="0.5" px-2 py-1 rounded-2 flex items-center justify-center gap-1 flex-inline transition="all duration-300"
+    box-border @click="onClick"
   >
     <div :class="props.iconClass" />
     <div select-none>
