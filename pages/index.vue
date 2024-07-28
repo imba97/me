fcfded04b8f28a92b70b996b2725d7816acb0144
imba97 <mail@imba97.cn>
2024-07-28 12:15:40 +0800

    feat: Home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <script lang="ts" setup>
-const router = useRouter()
-router.push('/resume')
+setTimeout(() => {
+  const router = useRouter()
+  router.push('/resume')
+}, 2000)
 </script>
 
 <template>
-  <div>index</div>
+  <div h-full w-full flex items-center justify-center>
+    <div flex="~ col" items-center gap-2 animate-fade-in>
+      <div h-32 w-32>
+        <img src="/favicon.png">
+      </div>
+      <div text-8 bg-clip-text text-transparent bg-gradient-to-tr from="#bd34fe" to="#47caff">
+        Hi
+      </div>
+    </div>
+  </div>
 </template>
