d677fe18e691fc965b96b4b3dde091201243edf1
imba97 <mail@imba97.cn>
2024-07-29 13:58:37 +0800

    perf: update company list

--- pages/resume/lists/Company.vue
+++ pages/resume/lists/Company.vue
@@ -10,8 +10,8 @@ import BaseList from './BaseList.vue'
       </div>
       <ol>
         <li>可视化打印模板编辑器</li>
-        <li>封装 Excel 表格导入导出工具，可快速对应前端表格组件</li>
-        <li>封装小程序相关脚本，一套代码打包、封装 WebView 通讯机制</li>
+        <li>封装 Excel 表格导入导出工具</li>
+        <li>编写小程序自动化脚本，一套代码打包、封装小程序 WebView 通讯机制</li>
       </ol>
     </li>
     <li>
@@ -19,11 +19,10 @@ import BaseList from './BaseList.vue'
         点餐屏跨端应用开发，皮爷咖啡、赛百味、泸溪河，使用 Capacitor 框架支持 Windows、Android、iOS
       </div>
       <ol>
-        <li>调研并对接电子秤、副屏、读卡器、支付 POS 机等硬件设备</li>
-        <li>调研并对接蓝牙打印机、网口打印机、Windows 打印机、DLL 打印机</li>
-        <li>编写 Capacitor 插件、封装前端工具库、封装零依赖新加坡 NETS 支付 SDK</li>
-        <li>封装 TSCLIB 前端库，大幅降低 Windows Electron 打印数据体积</li>
+        <li>调研并对接蓝牙打印机、网口打印机、Windows 打印机、DLL 打印机、电子秤、副屏、读卡器、支付 POS 机等硬件设备</li>
         <li>封装 Vue 弹窗，扩展性极强。按钮异步函数自动 loading、多按钮自动禁用、支持使用 JSX 完成复杂内容</li>
+        <li>编写 Capacitor 插件，Windows 和 iOS</li>
+        <li>封装前端工具库、封装零依赖新加坡 NETS 支付 SDK</li>
       </ol>
     </li>
   </BaseList>
