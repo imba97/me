6d347290ac0c0266ddeda4599244f69efb8d46ac
imba97 <mail@imba97.cn>
2024-07-30 13:08:48 +0800

    perf: Optimize display of homepage icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <script lang="ts" setup>
-setTimeout(() => {
-  const router = useRouter()
-  router.push('/resume')
-}, 2000)
+const showFavicon = ref(false)
+
+onMounted(() => {
+  const faviconImage = new Image()
+  faviconImage.src = '/favicon.png'
+
+  faviconImage.onload = () => {
+    showFavicon.value = true
+
+    setTimeout(() => {
+      const router = useRouter()
+      router.push('/resume')
+    }, 2000)
+  }
+})
 </script>
 
 <template>
   <div h-full w-full flex items-center justify-center>
-    <div flex="~ col" items-center gap-2 animate-fade-in>
+    <div v-if="showFavicon" flex="~ col" items-center gap-2 animate-fade-in>
       <div h-32 w-32>
         <img src="/favicon.png">
       </div>
