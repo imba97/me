2a38011cf88930764ebb36b7c43d4f5efa6f8640
imba97 <mail@imba97.cn>
2024-08-16 14:02:11 +0800

    feat: 精简工作项目介绍

--- pages/resume/lists/Company.vue
+++ pages/resume/lists/Company.vue
@@ -2,24 +2,16 @@
   <BaseList>
     <li>
       <div>
-        淘宝小程序商家助手，独立开发跨境电商应用跨境助手
+        <span>淘宝小程序商家助手，独立开发跨境电商应用跨境助手。</span>
+        <span>做了可视化打印模板编辑器、Excel 导入导出工具封装、小程序自动化脚本等</span>
       </div>
-      <ol>
-        <li>可视化打印模板编辑器</li>
-        <li>封装 Excel 表格导入导出工具</li>
-        <li>编写小程序自动化脚本，一套代码打包、封装小程序 WebView 通讯机制</li>
-      </ol>
     </li>
     <li>
       <div>
-        点餐屏跨端应用开发，皮爷咖啡、赛百味、泸溪河，使用 Capacitor 框架支持 Windows、Android、iOS
+        <span>点餐屏跨端应用开发，皮爷咖啡、赛百味、泸溪河，使用 Capacitor 框架支持 Windows、Android、iOS。</span>
+        <span>调研并对接蓝牙打印机、网口打印机、Windows 打印机、DLL 打印机、电子秤、副屏、读卡器、支付 POS 机等硬件设备。</span>
+        <span>封装零依赖 NETS 支付 SDK</span>
       </div>
-      <ol>
-        <li>调研并对接蓝牙打印机、网口打印机、Windows 打印机、DLL 打印机、电子秤、副屏、读卡器、支付 POS 机等硬件设备</li>
-        <li>封装 Vue 弹窗，扩展性极强。按钮异步函数自动 loading、多按钮自动禁用、支持使用 JSX 完成复杂内容</li>
-        <li>编写 Capacitor 插件，Windows 和 iOS</li>
-        <li>封装前端工具库、封装零依赖新加坡 NETS 支付 SDK</li>
-      </ol>
     </li>
   </BaseList>
 </template>
