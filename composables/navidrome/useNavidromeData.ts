785ce5ec2816ddeeae265fd7c8c8cfec8938de6f
imba97 <mail@imba97.cn>
2024-08-19 10:43:14 +0800

    fix: 没获取到最近在听的歌曲问题

--- composables/navidrome/useNavidromeData.ts
+++ composables/navidrome/useNavidromeData.ts
@@ -28,16 +28,61 @@ export default async function () {
 }
 
 async function getHistory() {
-  return await axiosInstance.post('/getAlbumList.view', null, mergeOptions({
+  const recent = await axiosInstance.post('/getAlbumList.view', null, mergeOptions({
     params: {
       type: 'recent',
       size: 1
     }
   }))
-    .then(res => _get(res.data, 'subsonic-response.albumList.album.0', []))
+    .then(res => _get(res.data, 'subsonic-response.albumList.album.0'))
+
+  if (!recent) {
+    return
+  }
+
+  return getAlbum(recent.id)
+    .then((songs) => {
+      if (songs.length === 0) {
+        return
+      }
+
+      // 获取最新
+      let recentMusic: any
+      let recentTime = 0
+
+      // 最近播放
+      _forEach(songs, (song) => {
+        if (!recentMusic) {
+          recentMusic = song
+          return
+        }
+
+        if (!_has(song, 'played')) {
+          return
+        }
+
+        const currentSongPlayed = new Date(song.played).getTime()
+
+        if (currentSongPlayed > recentTime) {
+          recentMusic = song
+          recentTime = currentSongPlayed
+        }
+      })
+
+      return recentMusic
+    })
 }
 
 async function getPlaying() {
   return await axiosInstance.post('/getNowPlaying.view', null, commonOptions)
     .then(res => _get(res.data, 'subsonic-response.nowPlaying.entry.0', null))
+}
+
+async function getAlbum(id: string) {
+  return await axiosInstance.post('/getAlbum.view', null, mergeOptions({
+    params: {
+      id
+    }
+  }))
+    .then(res => _get(res.data, 'subsonic-response.album.song', []))
 }
