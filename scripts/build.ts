c1dc184016e5654c7b9c22383cf72e07896278c8
imba97 <mail@imba97.cn>
2024-09-29 14:38:40 +0800

    chore: update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,9 @@
 import process from 'node:process'
 import { spawn } from 'node:child_process'
 
-const child = spawn('npx', ['nuxi', 'build'])
+const child = spawn('npx', ['nuxi', 'build'], {
+  env: process.env
+})
 
 const successCondition = [
   'npx nuxthub deploy',
