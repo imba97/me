d030bf8ebe4ee3632cb6a8c89f5c15d1784bfe3c
imba97 <mail@imba97.cn>
2024-09-29 14:48:34 +0800

    chore: test code

--- server/api/playing/test.ts
+++ server/api/playing/test.ts
@@ -3,6 +3,10 @@ import process from 'node:process'
 export default defineEventHandler(async () => {
   return {
     NAVIDROME_API_URL: process.env.NAVIDROME_API_URL,
-    env: process.env
+    types: {
+      NAVIDROME_API_URL: typeof process.env.NAVIDROME_API_URL
+    },
+    env: process.env,
+    envKeys: Object.keys(process.env)
   }
 })
