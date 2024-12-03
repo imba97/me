3d788300b78fc981e21a923aa8195bb39e30c7c8
imba97 <mail@imba97.cn>
2024-12-03 13:37:32 +0800

    perf: significantly optimize image size

--- composables/navidrome/useNavidromeCover.ts
+++ composables/navidrome/useNavidromeCover.ts
@@ -3,13 +3,20 @@ import { navidromeRequest } from './shared'
 
 export default async function (id: string) {
   const cover = await navidromeRequest.blob('/getCoverArt.view', {
-    id
+    id,
+    size: 300
   })
 
   if (!(cover instanceof Blob)) {
     return ''
   }
 
-  // TODO: 待测试
-  return sharp(await cover.arrayBuffer()).resize(300, 300).toBuffer()
+  const buffer = await cover.arrayBuffer()
+
+  const compressed = sharp(buffer).webp({
+    quality: 50,
+    effort: 6
+  }).toBuffer()
+
+  return compressed
 }
