cc29e93aeea89ac04374bf821ccdc6cdeff002a3
imba97 <mail@imba97.cn>
2024-12-05 17:19:17 +0800

    feat: cyberpunk resume page

---
+++ pages/resume/cyberpunk/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div size-full px-7 py-10 of-x-hidden bg-slate-950>
+    <div prose ma>
+      <div mb-6 fyc gap-4>
+        <div size-14>
+          <img src="/favicon.png">
+        </div>
+        <CyberpunkName />
+      </div>
+    </div>
+  </div>
+</template>
