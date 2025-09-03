c17b4266ef0157ccec14f37df23ed2e867fd3dbc
imba97 <mail@imba97.cn>
2025-09-03 21:53:30 +0800

    Revert "refactor: use lodash-imports"
    This reverts commit 37bf3967cdf7bcf8177b37c729396780a379bfcd.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,9 @@ import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 import IntroxdResolver from '@introxd/components/resolver'
-import LodashImports from 'lodash-imports'
-import Components from 'unplugin-vue-components/vite'
+import components from 'unplugin-vue-components/vite'
 
 const r = (path: string) => fileURLToPath(new URL(path, import.meta.url))
-
-const { imports: lodashAutoImports } = LodashImports({ hasFrom: true })
 
 export default defineNuxtConfig({
   modules: [
@@ -21,6 +18,12 @@ export default defineNuxtConfig({
 
   image: {
     provider: process.env.NODE_ENV === 'production' ? 'netlify' : 'ipx'
+  },
+
+  lodash: {
+    prefix: '_',
+    prefixSkip: false,
+    upperAfterPrefix: false
   },
 
   css: [
@@ -38,8 +41,7 @@ export default defineNuxtConfig({
       ].map(name => ({
         name,
         from: '@introxd/components'
-      })),
-      ...lodashAutoImports
+      }))
     ]
   },
 
@@ -66,7 +68,7 @@ export default defineNuxtConfig({
 
   vite: {
     plugins: [
-      Components({
+      components({
         dts: r('./.nuxt/lib-components.d.ts'),
         resolvers: [
           IntroxdResolver()
