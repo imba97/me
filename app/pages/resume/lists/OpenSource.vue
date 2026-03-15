be53309166dfa16b734ce0e969553ac97f30e273
imba97 <mail@imba97.cn>
2026-03-15 18:34:30 +0800

    feat: add `h2l-ranking` description

--- app/pages/resume/lists/OpenSource.vue
+++ app/pages/resume/lists/OpenSource.vue
@@ -33,6 +33,30 @@
 
     <li>
       <div>
+        <span>「从夯到拉」排行榜</span>
+        <IxIconText :preset="IconTextPreset.Github" href="imba97/h2l-ranking">
+          h2l-ranking
+        </IxIconText>
+        <span> Vue3 组件，用于我的</span>
+        <IxIconText
+          icon-class="i-solar-ranking-bold-duotone size-6 bg-colorful"
+          href="/ranking"
+        >
+          <span text-colorful>个人排行</span>
+        </IxIconText>
+        <span>，以及做了一个</span>
+        <IxIconText
+          icon-class="i-mingcute-link-2-line"
+          href="https://h2l-ranking.netlify.app/"
+        >
+          网站
+        </IxIconText>
+        <span>可以上传图片通过拖拽做自己的排行榜</span>
+      </div>
+    </li>
+
+    <li>
+      <div>
         <span>免安装脚本</span>
         <IxIconText :preset="IconTextPreset.Github" href="imba97/iconfontx">
           iconfontx
